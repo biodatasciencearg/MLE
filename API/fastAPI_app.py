01e6fda53579694fba57b3e33a6da5fe203a2ddc
Elias Lopez <elias.lopez@ar.mcd.com>
2021-12-24 00:00:53 -0300

    reorder project first commit

---
+++ API/fastAPI_app.py
@@ -0,0 +1,99 @@
+import uvicorn
+import requests
+import json
+from fastapi import FastAPI
+import time
+import sqlite3
+import pandas as pd
+
+
+# defino la url donde estara la api.
+url =  "http://127.0.0.1:8000"
+
+description = """
+KueskiCreditRisk API helps you evaluate clients. Computing Default probability or getting features/descriptors. 🚀
+
+## Online Feature Store
+
+You can **get features** for each client with a client_id.
+
+## Credit Risk Score
+
+You will be able to:
+
+* **Default Probability**: return default probability and the Label o/1 given a user ID.
+
+"""
+tags_metadata = [
+    {
+        "name": "credit_risk",
+        "description": "Retrieve probability of Default using a Random Forest estimator.",
+    },
+    {
+        "name": "online_feature_store",
+        "description": "the API gets 5 variables import to define the default probability.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://kueski_documentation.com/",
+        },
+    },
+]
+
+app = FastAPI(title="Kueski CreditRisk",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "eliasLopez@developer.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },openapi_tags=tags_metadata)
+
+from predictor import MyKueskiModel
+
+# Read/loads model in memory. Waiting requets.
+model = MyKueskiModel()
+
+# Endopoint credit risk.
+@app.get("/credit_risk/client_id={id}",tags=["credit_risk"])
+def home(id: int):
+  # consulta a base de datos para ver si existe el client_id con respecto a si tiene o no features.
+  # control sobre timeouts posible caida en servicio de feature store.
+  response = requests.get(url+f"/online_feature_store/client_id={id}")
+  print(response.json())
+  print(type(response.json()))
+  features = pd.DataFrame(response.json(),index=[0]).drop(columns='id')
+  label, proba = model.predict(features)
+  # consulta a base de datos para ver si existe el client_id con respecto a si tiene o no features.
+  return {"client_id": id,"label":label, "predict_proba":proba}
+
+
+
+@app.get("/online_feature_store/client_id={id}",tags=['online_feature_store'])
+def home(id: int):
+  # creo la conexion.
+  cnx = sqlite3.connect('feature_store_online.db')
+  id = 5008805
+  query = f"""SELECT   age
+                   , years_on_the_job
+                   , nb_previous_loans
+                   , avg_amount_loans_previous
+                   , flag_own_car
+            FROM features_table where id = {id}"""
+  features = pd.read_sql_query(query, cnx)
+  # Lo mando a un diccionario.
+  features = features.iloc[0].to_dict()
+  # Agrego el id
+  features['id'] = id
+  # cierro la conexion.
+  cnx.close()
+  # devuelvo el json con las features y el id.
+  return features
+  
+
+if __name__ == "__main__":
+    uvicorn.run("fastAPI_app:app")
---
+++ API/predictor.py
@@ -0,0 +1,13 @@
+from joblib import load
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+class MyKueskiModel():
+    def __init__(self):
+        """Cuando instancio el objeto y empieza a correr la API solo levanto una vez el modelo y 
+        respondo frente  a los llamados."""
+        with open('model_risk.joblib', 'rb') as handle:
+            self.model = load(handle)
+        
+    def predict(self,array):
+        return (self.model.predict(array),self.model.predict_proba(array))
